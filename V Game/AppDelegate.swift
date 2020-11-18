9f990b6c37aed71842e8d6018be7de26fcd3f725
Maksim Gritskov <grickov@me.com>
2020-11-18 17:15:26 +0300

    Initial Commit

---
+++ V Game/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  V Game
+//
+//  Created by Максим Грицков on 18.11.2020.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
